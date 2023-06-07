037e59e673d0c4f3b2087d3e6115bcfa239b3c5d
lineyha <yasir_duman@hotmail.com>
2023-06-07 16:08:05 +0300

    routerlar düzeltildi

--- ioes/ioes/src/components/screens/BecameACandidate.js
+++ ioes/ioes/src/components/screens/BecameACandidate.js
@@ -1,9 +1,25 @@
+import logo from './iyteee.png';
+import { Link } from 'react-router-dom';
 function BecameACandidate(){
 
     return(
 
         <>
-        Became A Candidate
+       
+       <div className="main-container">
+    <header className="header-container">
+      <h1 className="header-title">IZTECH STUDENT COUNCIL ELECTION SYSTEM</h1>
+      <img src={logo} alt="Logo" className="logo" />
+    </header>
+    <div className="content-container">
+      <div className="sidebar">
+
+        <Link to="/student-main/election-status">Election Status</Link>
+        <Link to="/student-main/election-result">Election Result</Link>
+        <Link to ="/student-main/help">Help</Link>
+      </div>
+    </div>
+  </div>
         
         </>
     );
