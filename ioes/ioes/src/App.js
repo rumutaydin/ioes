194c2fe2862e002987c9265a99579f1e03c2a374
rumeysacoskunn <rumeysacoskun80@gmail.com>
2023-06-08 00:23:59 +0300

    app

--- ioes/ioes/src/App.js
+++ ioes/ioes/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import StudentLogin from './components/screens/StudentLogin';
-import AdminLogin from './components/screens/StudentLogin';
+import AdminLogin from './components/screens/AdminLogin';
 import StudentMainPage from './components/screens/StudentMainPage';
 import AdminMainPage from './components/screens/AdminMainPage';
-import { BrowserRouter,Routes, Route, createBrowserRouter, createRoutesFromElements,RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes ,createRoutesFromElements,RouterProvider, createBrowserRouter} from 'react-router-dom';
 import Blank from './components/screens/Blank';
 import CastVote from './components/screens/CastVote';
 import BecameACandidate from './components/screens/BecameACandidate';
@@ -36,11 +36,32 @@ const router = createBrowserRouter(
 )
 
 
+import SetTimeDatePage from './components/screens/SetTimeDatePage';
+import Option from './components/screens/2-ilksayfa';               ////////////////////////////// aşağıda studentlogin rootsu değiştirdim
 
 function App() {
+  const isLoggedIn = !!localStorage.getItem('token'); ///////////////
+
   return (
-    <RouterProvider router={router}/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<StudentLogin />} />
+        <Route path="/admin-login" element={<AdminLogin/>} /> 
+        <Route path="/student-main" element={<StudentMainPage />} />
+        <Route path="/admin-main" element={<AdminMainPage />} />
+        <Route path='/become-candidate' element={<Blank />} />
+        <Route path='/cast-vote' element={<CastVote />} />
+        <Route path='/election-status' element={<Blank />} />
+        <Route path='/election-results' element={<Blank />} />
+        <Route path='/election-help' element={<Blank />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+
+
