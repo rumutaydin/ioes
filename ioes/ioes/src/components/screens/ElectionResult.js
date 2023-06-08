20deed02b55300c26e9a191d0f4210f326548903
rumeysacoskunn <rumeysacoskun80@gmail.com>
2023-06-09 01:08:36 +0300

    Update ElectionResults.js
    Update ElectionResults.js

--- ioes/ioes/src/components/screens/ElectionResult.js
+++ ioes/ioes/src/components/screens/ElectionResult.js
@@ -12,115 +12,114 @@ const ElectionResult = () => {
   const [winner, setWinner] = useState(null);
 
 
-useEffect(() => {
-  // Fetch candidates when the department number is available
-  fetchCandidates();
-  fetchElection();
-  //checkVotingStat();
-
-}, []);
-
-const fetchCandidates = async () => {
-  try {
-    const token = localStorage.getItem('token');
-    const candidatesResponse = await axios.get('http://localhost:8080/api/candidates', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    // Extract the candidates and pics from the response
-    const { candidates } = candidatesResponse.data;
-    setCandidates(candidates);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const fetchElection = async () => {
-  try {
-    const token = localStorage.getItem('token');
-    const electionResponse = await axios.get('http://localhost:8080/api/getElection', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    const {electionEndDate} = electionResponse.EndDate;
-    setEndDate(electionEndDate);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-
-useEffect(() => {
-  if (endDate && new Date() >= endDate) {
-    setIsElectionFinished(true);
+  useEffect(() => {
+    // Fetch candidates when the department number is available
+    fetchCandidates();
+    fetchElection();
     findWinner();
-  }
-}, [endDate]);
 
-const findWinner = () => {
-  if (candidates.length === 0) {
-    setWinner(null);
-    return;
-  }
+  }, []);
 
-  let maxVotes = candidates[0].countVote;
-  let winnerIndex = 0;
+  const fetchCandidates = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const candidatesResponse = await axios.get('http://localhost:8080/api/candidates', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
-  for (let i = 1; i < candidates.length; i++) {
-    if (candidates[i].countVote > maxVotes) {
-      maxVotes = candidates[i].countVote;
-      winnerIndex = i;
+      // Extract the candidates and pics from the response
+      const { candidates } = candidatesResponse.data;
+      setCandidates(candidates);
+    } catch (error) {
+      console.error(error);
     }
-  }
+  };
 
-  setWinner(candidates[winnerIndex]);
-};
+  const fetchElection = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const electionResponse = await axios.get('http://localhost:8080/api/getElection', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const { electionEndDate } = electionResponse.EndDate;
+      setEndDate(electionEndDate);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  useEffect(() => {
+    if (endDate && new Date() >= endDate) {
+      setIsElectionFinished(true);
+      findWinner();
+    }
+  }, [endDate]);
+
+
+  const findWinner = () => {
+    if (candidates.length === 0) {
+      setWinner(null);
+      return;
+    }
+
+    let maxVotes = candidates[0].countVote;
+    let winnerIndex = 0;
+
+    for (let i = 1; i < candidates.length; i++) {
+      if (candidates[i].countVote > maxVotes) {
+        maxVotes = candidates[i].countVote;
+        winnerIndex = i;
+      }
+    }
+
+    setWinner(candidates[winnerIndex]);
+  };
 
 
 
-
-
-
-    return(
-
-        <>
-        
-        <div className="main-container">
-    <header className="header-container">
-      <h1 className="header-title">IZTECH STUDENT COUNCIL ELECTION SYSTEM</h1>
-      <img src={logo} alt="Logo" className="logo" />
-    </header>
-    <div className="content-container">
-      <div className="sidebar">
-        <Link to="/student-main">Student Main</Link>
-        <Link to="/student-main/became-candidate">Become a Candidate</Link>
-        <Link to="/student-main/cast-vote">Cast Vote</Link>
-        <Link to="/student-main/election-status">Election Status</Link>
-        <Link to ="/student-main/help">Help</Link>
-      </div>
-    </div>
-  </div>
-       
-  <div>
-      {isElectionFinished ? (
-        winner ? (
-          <div>
-            <h2>Winner: {winner.name}</h2>
-            <p>Votes: {winner.countVote}</p>
-          </div>
+return (
+    <>
+      <div>
+        {isElectionFinished ? (
+          winner ? (
+            <div>
+              <h2>Winner: {winner.name}</h2>
+              <p>Votes: {winner.countVote}</p>
+            </div>
+          ) : (
+            <h2>No winner found.</h2>
+          )
         ) : (
-          <h2>No winner found.</h2>
-        )
-      ) : (
-        <h2>The election has not ended yet.</h2>
-      )}
-    </div>
-        </>
-    );
-    
+          <h2>The election has not ended yet.</h2>
+        )}
+      </div>
+
+      <div className="main-container">
+        <header className="header-container">
+          <h1 className="header-title">IZTECH STUDENT COUNCIL ELECTION SYSTEM</h1>
+          <img src={logo} alt="Logo" className="logo" />
+        </header>
+        <div className="content-container">
+          <div className="sidebar">
+            <Link to="/student-main">Student Main</Link>
+            <Link to="/student-main/became-candidate">Become a Candidate</Link>
+            <Link to="/student-main/cast-vote">Cast Vote</Link>
+            <Link to="/student-main/election-status">Election Status</Link>
+            <Link to="/student-main/help">Help</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+
 }
 export default ElectionResult;
+
+
+
