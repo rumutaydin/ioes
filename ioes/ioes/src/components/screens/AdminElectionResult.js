03412939705c854c144f33fa3cf20c5f03cd3371
lineyha <yasir_duman@hotmail.com>
2023-06-09 17:37:26 +0300

    admin election result biraz düzenleme

--- ioes/ioes/src/components/screens/AdminElectionResult.js
+++ ioes/ioes/src/components/screens/AdminElectionResult.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import logo from './iyteee.png';
+import { Link } from 'react-router-dom';
 
 const AdminElectionResult = () => {
   const [winners, setWinners] = useState([]);
@@ -26,7 +28,25 @@ const AdminElectionResult = () => {
   };
 
   return (
-    <div>
+
+    <>
+
+<div className="main-container">
+        <header className="header-container">
+          <h1 className="header-title">IZTECH STUDENT COUNCIL ELECTION SYSTEM</h1>
+          <img src={logo} alt="Logo" className="logo" />
+        </header>
+        <div className="content-container">
+          <div className="sidebar">
+            <Link to="/student-main">Student Main</Link>
+            <Link to="/student-main/became-candidate">Become a Candidate</Link>
+            <Link to="/student-main/cast-vote">Cast Vote</Link>
+            <Link to="/student-main/election-status">Election Status</Link>
+            <Link to="/student-main/help">Help</Link>
+          </div>
+
+
+          <div>
       {winners.length > 0 ? (
         winners.map((winner, index) => (
           <div key={index}>
@@ -39,6 +59,16 @@ const AdminElectionResult = () => {
         <h2>Refresh the page if you can not see the winners.</h2>
       )}
     </div>
+
+          </div>
+</div>
+
+
+
+    
+    
+    
+    </>
   );
 };
 
