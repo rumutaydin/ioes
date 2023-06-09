77c4d01a622d95c6964e82e5b9196fba70d4c635
eren.demirell <eren27demirel@hotmail.com>
2023-06-09 15:28:54 +0300

    Student,Admin Election Result
    Student,Admin Election Result

--- ioes/ioes/src/components/screens/AdminElectionResult.js
+++ ioes/ioes/src/components/screens/AdminElectionResult.js
@@ -1,36 +1,45 @@
-import logo from './iyteee.png';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function AdminElectionResult(){
+const AdminElectionResult = () => {
+  const [winners, setWinners] = useState([]);
 
-    return(
+  useEffect(() => {
+    const getWinners = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/api/winners`);
+        const winnersData = response.data;
+        setWinners(winnersData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        <>
-        
-        <div className="main-container">
-    <header className="header-container">
-      <h1 className="header-title">IZTECH STUDENT COUNCIL ELECTION SYSTEM</h1>
-      <img src={logo} alt="Logo" className="logo" />
-    </header>
-    <div className="content-container">
-      <div className="sidebar">
+    getWinners();
+  },[]);
 
-          <Link to="/admin-main">Admin Main</Link>
-          <Link to="/admin-main/set-time-date">Set Election Time and Date</Link>
-          <Link to="/admin-main/see-applications">See Candidate Applications</Link>
-          <Link to="/admin-main/election-help">Help</Link>
-          <Link to="/">Log Out</Link>
+  const departmentMapping = {
+    0: 'Computer Engineering',
+    1: 'Chemical Engineering',
+    2: 'Electronics and Communication Engineering',
+    3: 'Mechanical Engineering',
+  };
 
-
-
-       
-      </div>
+  return (
+    <div>
+      {winners.length > 0 ? (
+        winners.map((winner, index) => (
+          <div key={index}>
+            <h2>Department: {departmentMapping[winner.deptNo]}</h2>
+            <h2>Winner: {winner.name}</h2>
+            <p>Votes: {winner.countVote}</p>
+          </div>
+        ))
+      ) : (
+        <h2>Refresh the page if you can not see the winners.</h2>
+      )}
     </div>
-  </div>
-       
-        
-        </>
-    );
-}
+  );
+};
 
 export default AdminElectionResult;
--- ioes/ioes/src/components/screens/StudentElectionResult.js
+++ ioes/ioes/src/components/screens/StudentElectionResult.js
@@ -1,32 +1,45 @@
-import logo from './iyteee.png';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function StudentElectionResult(){
+const AdminElectionResult = () => {
+  const [winners, setWinners] = useState([]);
 
-    return(
+  useEffect(() => {
+    const getWinners = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/api/winners`);
+        const winnersData = response.data;
+        setWinners(winnersData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        <>
-        
-        <div className="main-container">
-    <header className="header-container">
-      <h1 className="header-title">IZTECH STUDENT COUNCIL ELECTION SYSTEM</h1>
-      <img src={logo} alt="Logo" className="logo" />
-    </header>
-    <div className="content-container">
-      <div className="sidebar">
-        <Link to="/student-main">Student Main</Link>
-        <Link to="/student-main/became-candidate">Become a Candidate</Link>
-        <Link to="/student-main/cast-vote">Cast Vote</Link>
-        <Link to="/student-main/election-status">Election Status</Link>
-        <Link to ="/student-main/help">Help</Link>
-        <Link to="/">Log Out</Link>
-      </div>
+    getWinners();
+  },[]);
+
+  const departmentMapping = {
+    0: 'Computer Engineering',
+    1: 'Chemical Engineering',
+    2: 'Electronics and Communication Engineering',
+    3: 'Mechanical Engineering',
+  };
+
+  return (
+    <div>
+      {winners.length > 0 ? (
+        winners.map((winner, index) => (
+          <div key={index}>
+            <h2>Department: {departmentMapping[winner.deptNo]}</h2>
+            <h2>Winner: {winner.name}</h2>
+            <p>Votes: {winner.countVote}</p>
+          </div>
+        ))
+      ) : (
+        <h2>Refresh the page if you can not see the winners.</h2>
+      )}
     </div>
-  </div>
-       
-        
-        </>
-    );
-}
+  );
+};
 
-export default StudentElectionResult;
+export default AdminElectionResult;
