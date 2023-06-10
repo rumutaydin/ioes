1ff410655519c0ae95c3a700270d908f189b0670
Robin Umut Aydın <aydinrobinumut@gmail.com>
2023-06-10 22:05:49 +0300

    election results

--- ioes/ioes/src/components/screens/StudentElectionResult.js
+++ ioes/ioes/src/components/screens/StudentElectionResult.js
@@ -11,15 +11,15 @@ const AdminElectionResult = () => {
     const getWinners = async () => {
       try {
         const response = await axios.get(`http://localhost:8080/api/winners`);
-        const winnersData = response.data;
-        setWinners(winnersData);
+        const { winners } = response.data;
+        setWinners(winners);
       } catch (error) {
         console.error(error);
       }
     };
 
     getWinners();
-  },[]);
+  }, []);
 
   const departmentMapping = {
     0: 'Computer Engineering',
@@ -29,57 +29,36 @@ const AdminElectionResult = () => {
   };
 
   return (
-
-
-
     <>
-    
-
-    <div className="main-container">
-    <header className="header-container">
-      <h1 className="header-title">IZTECH STUDENT COUNCIL ELECTION SYSTEM</h1>
-      <img src={logo} alt="Logo" className="logo" />
-    </header>
-    <div className="content-container">
-      <div className="sidebar">
-        <Link to="/student-main">Student Main</Link>
-        <Link to="/student-main/became-candidate">Become a Candidate</Link>
-        <Link to="/student-main/cast-vote">Cast Vote</Link>
-        <Link to="/student-main/election-status">Election Status</Link>
-        <Link to="/student-main/election-result">Election Result</Link>
-      </div>
-
-      <div>
-      {winners.length > 0 ? (
-        winners.map((winner, index) => (
-          <div key={index} className='winnders-item'>
-            <h2>Department: {departmentMapping[winner.deptNo]}</h2>
-            <h2 className='winnder-name'>Winner: {winner.name}</h2>
-            <p className='vote-count'>Votes: {winner.countVote}</p>
+      <div className="main-container">
+        <header className="header-container">
+          <h1 className="header-title">IZTECH STUDENT COUNCIL ELECTION SYSTEM</h1>
+          <img src={logo} alt="Logo" className="logo" />
+        </header>
+        <div className="content-container">
+          <div className="sidebar">
+            <Link to="/student-main">Student Main</Link>
+            <Link to="/student-main/became-candidate">Become a Candidate</Link>
+            <Link to="/student-main/cast-vote">Cast Vote</Link>
+            <Link to="/student-main/election-status">Election Status</Link>
+            <Link to="/student-main/election-result">Election Result</Link>
           </div>
-        ))
-      ) : (
-        <h2>Refresh the page if you can not see the winners.</h2>
-      )}
-    </div>
 
-
-    </div>
-  </div>
-
-
-
-
-
-
-
-    
-
-
-
-
-
-    
+          <div>
+            {winners.length > 0 ? (
+              winners.map((winner, index) => (
+                <div key={index} className="winners-item">
+                  <h2>Department: {departmentMapping[winner.deptNo]}</h2>
+                  <h2 className="winner-name">Winner: {winner.name}</h2>
+                  <p className="vote-count">Votes: {winner.countVote}</p>
+                </div>
+              ))
+            ) : (
+              <h2>Refresh the page if you cannot see the winners.</h2>
+            )}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
