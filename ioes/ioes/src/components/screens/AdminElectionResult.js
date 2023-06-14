194fd02d332351b3ec4162574f76cf14cd4c11c6
lineyha <yasir_duman@hotmail.com>
2023-06-14 13:59:12 +0300

    help

--- ioes/ioes/src/components/screens/AdminElectionResult.js
+++ ioes/ioes/src/components/screens/AdminElectionResult.js
@@ -43,7 +43,7 @@ const AdminElectionResult = () => {
               <Link to="/admin-main/set-time-date">Set Election Time and Date</Link>
               <Link to="/admin-main/see-applications">See Candidate Applications</Link>
               <Link to="/admin-main/election-results">Election Results</Link>
-              <Link to="/admin-main/election-help">Help</Link>
+              <Link to="/admin-main/admin-help">Help</Link>
               <Link to="/">Log Out</Link>
 
           </div>
