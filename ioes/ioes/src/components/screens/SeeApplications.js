209fed58bb6f6088ae02cdbc91f859fdffb9c6db
lineyha <yasir_duman@hotmail.com>
2023-06-14 14:06:00 +0300

    help

--- ioes/ioes/src/components/screens/SeeApplications.js
+++ ioes/ioes/src/components/screens/SeeApplications.js
@@ -77,7 +77,7 @@ function SeeApplications() {
               <Link to="/admin-main/set-time-date">Set Election Time and Date</Link>
               <Link to="/admin-main/see-applications">See Candidate Applications</Link>
               <Link to="/admin-main/election-results">Election Results</Link>
-              <Link to="admin-main/election-help">Help</Link>
+              <Link to="/admin-main/admin-help">Help</Link>
               <Link to="/">Log Out</Link>
     
             </div>
